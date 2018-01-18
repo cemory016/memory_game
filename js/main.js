e779c503dee8792e406132fc614f328e4dc1e4f6
Carmen <cemory016@gmail.com>
2018-01-17 23:52:20 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,19 +7,41 @@ console.log("User Flipped" + cardOne);
 console.log("User Flipped" + cardThree);
 */
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-
-var cardOne = cards[0];
+/*var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
-
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("you found a match!");
 }
 else {
 	alert("Sorry, try again.");
+}*/
+
+/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("sorry, try again.");
+	}
 }
+
+var flipCard = function(cardId) {
+	console.log("user flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
