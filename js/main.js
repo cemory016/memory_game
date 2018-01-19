8f3084acca289c0c53bed48aad96c2f79e56f507
Carmen <cemory016@gmail.com>
2018-01-19 00:29:14 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -52,20 +52,37 @@ var cards = [
 var cardsInPlay = []; 
 
 var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-	} else {
-	alert("sorry, try again.");
+	console.log(this);
+	if (cardsInPlay.length === 2) {
+
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("sorry, try again.");
+    	}
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	console.log(this);
+	var cardId = this.getAttribute('data-id');
 	console.log("user flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 	
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+createBoard();
